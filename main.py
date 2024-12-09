a752644191f3e744d88605eb358440d881adb293
zay0lol <zynahisthebest2012@gmail.com>
2024-12-09 08:46:59 -0800

    operators and data type

---
+++ main.py
@@ -0,0 +1,23 @@
+a= int(input("Enter your first number"))
+b= float(input("Enter your second number which is decimal"))
+c= a+b
+print(c)
+name= "John Cena"
+age= 84
+is_boxer = True
+weight = 9999999.0
+print(type(name))
+print(type(age))
+print(type(is_boxer))
+print(type(weight))
+number2= "1"
+print(type(number2))
+hi= int(number2)
+print(type(hi))
+a= 14
+b = 16
+c= a
+a = b
+b = c
+print("Value of b is now",b)
+print("Value of a is now",a)
